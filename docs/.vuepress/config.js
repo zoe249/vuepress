f0f2e0873106e1f37adb500a42cdf18abb0bab74
王富贵 <1421759951@qq.com>
2021-09-02 10:27:27 +0800

    Vssue评论

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -44,14 +44,25 @@ module.exports = {
             ]
         }
     },
-    plugins: [
-        '@vuepress/pwa', {
+    plugins: {
+        '@vuepress/pwa': {
             serviceWorker: true,
             updatePopup: {
                 message: "发现新内容可用",
                 buttonText: "刷新"
             }
         },
+        '@vssue/vuepress-plugin-vssue': {
+            // 设置 `platform` 而不是 `api`
+            platform: 'github-v4',
+
+            // 其他的 Vssue 配置
+            owner: 'zoe249',
+            repo: 'vuepress',
+            clientId: 'ee504f8100edfb46fef4',
+            clientSecret: 'a06b64bc8e9d4218de8b81fdca679b95884e4d43',
+            autoCreateIssue: 'true'
+        }
         // [
         //     '@vuepress/last-updated',
         //     {
@@ -62,7 +73,7 @@ module.exports = {
         //         }
         //     }
         // ],
-    ]
+    }
 }
 
 // sidebar: [
